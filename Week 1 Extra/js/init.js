cd4f1e9ff71b940212887e30002cfbd4a7ac09b5
pwinnie2026 <paigewu.business@gmail.com>
2024-07-01 15:57:53 -0700

    add files

--- Week 1 Extra/js/init.js
+++ Week 1 Extra/js/init.js
@@ -1,7 +1,7 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=JWrMVIrr3Jz2WGVMeDwh', // Your style URL
     center: [ -118.444, 34.0709], // Starting position [lng, lat]
     zoom: 15 // Starting zoom level
 });
@@ -11,4 +11,12 @@ new maplibregl.Marker()
     .setLngLat([ -118.444, 34.0709])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
         .setHTML('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I used to work in '))
+    .addTo(map);
+
+let longitude = -118.45;
+let latitude = 34.05;
+
+// Add a marker to the map
+const marker = new maplibregl.Marker()
+    .setLngLat([longitude, latitude])
     .addTo(map);
