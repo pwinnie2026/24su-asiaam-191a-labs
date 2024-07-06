9580490881fe0e022e0d40c0acf65c012e343573
pwinnie2026 <paigewu.business@gmail.com>
2024-07-05 18:42:59 -0700

    new hover feature in js

--- Week2Extra/js/init.js
+++ Week2Extra/js/init.js
@@ -2,17 +2,31 @@
 const map = new maplibregl.Map({
     container: 'map', // container ID
     style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
-    center: [96.913707, 20.562050], // Starting position [lng, lat]
-    zoom: 9 // Starting zoom level
+    center: [-118.082272, 33.856938], // Starting position [lng, lat]
+    zoom: 10 // Starting zoom level
 });
 
 function addMarker(lat,lng,title,message){
     let popup_message = `<h2>${title}</h2> <h3>${message}</h3>`
-    new maplibregl.Marker()
+    let popup = new maplibregl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    }).setHTML(popup_message);
+
+    let marker = new maplibregl.Marker()
         .setLngLat([lng, lat])
         .setPopup(new maplibregl.Popup()
             .setHTML(popup_message))
         .addTo(map)
+
+    marker.getElement().addEventListener('mouseenter', () => {
+        popup.setLngLat([lng, lat]).addTo(map);
+    });
+        
+    marker.getElement().addEventListener('mouseleave', () => {
+        popup.remove();
+    });
+
     createButtons(lat,lng,title);
     return message
 }
@@ -31,6 +45,6 @@ function createButtons(lat,lng,title){
     document.getElementById("contents").appendChild(newButton); //(7)! 
 }
 
-addMarker(20.562050, 96.913707, "Inle Lake", "A childhood favorite spot!")
-addMarker(34.901524, -111.826171, "Capitol Hill", "My most recent trip!")
-addMarker(38.88856576879683, -77.00468020584017, "Sedona", "A new family favorite!")
+addMarker(33.856938, -118.082272, "Pho Tasty", "A family favorite! But no... she's not here!")
+addMarker(33.846493, -117.980919, "Martinez Nursery", "She's buying ANOTHER snake plant! At least we found her...")
+addMarker(33.745125, -117.960121, "Da Vien", "You'd think... but no, try checking another spot?")
